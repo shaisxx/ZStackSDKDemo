70b4225df6661b4eee9dd777fd61a089324d5f6b
shaisxx <shihu2010@foxmail.com>
2017-12-04 16:28:11 +0800

    网络-网络服务相关接口

--- src/com/shtd/zstack/network/NetworkServiceAction.java
+++ src/com/shtd/zstack/network/NetworkServiceAction.java
@@ -1,32 +1,64 @@
 package com.shtd.zstack.network;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.zstack.sdk.GetNetworkServiceTypesAction;
+import org.zstack.sdk.NetworkServiceProviderInventory;
+import org.zstack.sdk.QueryNetworkServiceProviderAction;
 
 import com.shtd.zstack.utils.ZStackUtils;
 
 /**
- * 网络-网络服务相关接口
+ * 5 网络-网络服务相关接口
  * @author Josh
  * @date 2017-11-29
  */
 public class NetworkServiceAction {
 
 	/**
-	 * 获取网络服务类型
+	 * 5.4.1 获取网络服务类型
 	 * @param sessionId
 	 * @return
 	 * @author Josh
 	 */
+	@SuppressWarnings("unchecked")
 	public GetNetworkServiceTypesAction.Result getNetworkServiceTypes(String sessionId){
 		GetNetworkServiceTypesAction action = new GetNetworkServiceTypesAction();
 		action.sessionId = sessionId;
 		GetNetworkServiceTypesAction.Result res = action.call();
 		if (res.error != null) {
 			System.out.println(String.format("getNetworkServiceTypes error code:%s,description:%s,details:%s", res.error.code, res.error.description, res.error.details));
+		} else {
+			Map<String, Object> types = res.value.types;
+			for (Entry<String, Object> entry: types.entrySet()) {
+			    System.out.println(String.format("getNetworkServiceTypes successfully key:%s,value:%s", entry.getKey(), entry.getValue()));
+			}
+		}
+		return res;
+	}
+	
+	/**
+	 * 5.4.2 查询网络服务模块
+	 * @param sessionId
+	 * @return
+	 * @author Josh
+	 */
+	public QueryNetworkServiceProviderAction.Result queryNetworkServiceProvider(String sessionId){
+		QueryNetworkServiceProviderAction action = new QueryNetworkServiceProviderAction();
+		action.conditions = Arrays.asList();
+		action.sessionId = sessionId;
+		QueryNetworkServiceProviderAction.Result res = action.call();
+		if (res.error != null) {
+			System.out.println(String.format("queryNetworkServiceProvider error code:%s,description:%s,details:%s", res.error.code, res.error.description, res.error.details));
+		} else {
+			for (int i = 0; i < res.value.getInventories().size(); i++) {
+				NetworkServiceProviderInventory inventory = res.value.getInventories().get(i);
+				System.out.println(String.format("queryNetworkServiceProvider successfully name:%s,uuid:%s", inventory.name, inventory.uuid));
+			}
 		}
 		return res;
 	}
