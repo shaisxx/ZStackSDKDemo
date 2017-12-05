a26933afb741af9602380d33bc3f96655640fe46
shaisxx <shihu2010@foxmail.com>
2017-12-05 13:42:08 +0800

    7 平台管理-控制台相关接口

--- src/com/shtd/zstack/platformmgt/ConsoleAction.java
+++ src/com/shtd/zstack/platformmgt/ConsoleAction.java
@@ -1,19 +1,26 @@
 package com.shtd.zstack.platformmgt;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.zstack.sdk.ConsoleInventory;
+import org.zstack.sdk.ConsoleProxyAgentInventory;
+import org.zstack.sdk.QueryConsoleProxyAgentAction;
+import org.zstack.sdk.ReconnectConsoleProxyAgentAction;
 import org.zstack.sdk.RequestConsoleAccessAction;
 
 import com.shtd.zstack.utils.ZStackUtils;
 
 /**
- * 平台管理-控制台相关接口
+ * 7 平台管理-控制台相关接口
  * @author Josh
  * @date 2017-11-29
  */
 public class ConsoleAction {
 
 	/**
-	 * 请求控制台访问地址
+	 * 7.4.1 请求控制台访问地址
 	 * @param vmInstanceUuid
 	 * @param sessionId
 	 * @return
@@ -30,6 +37,54 @@ public class ConsoleAction {
 			ConsoleInventory inventory = res.value.inventory;
 			System.out.println(String.format("requestConsoleAccess successfully scheme:%s,hostname:%s,port:%s,token:%s", 
 					inventory.scheme, inventory.hostname, inventory.port, inventory.token));
+		}
+		return res;
+	}
+	
+	/**
+	 * 7.4.2 查询控制台代理
+	 * @param uuid
+	 * @param sessionId
+	 * @return
+	 * @author Josh
+	 */
+	public QueryConsoleProxyAgentAction.Result queryConsoleProxyAgent(String uuid, String sessionId){
+		QueryConsoleProxyAgentAction action = new QueryConsoleProxyAgentAction();
+		action.conditions = Arrays.asList("uuid=" + uuid);
+		action.sessionId = sessionId;
+		QueryConsoleProxyAgentAction.Result res = action.call();
+		if (res.error != null) {
+			System.out.println(String.format("queryConsoleProxyAgent error code:%s,description:%s,details:%s", res.error.code, res.error.description, res.error.details));
+		} else {
+			for (int i = 0; i < res.value.getInventories().size(); i++) {
+				ConsoleProxyAgentInventory inventory = res.value.getInventories().get(i);
+				System.out.println(String.format("queryConsoleProxyAgent successfully consoleProxyOverriddenIp:%s,uuid:%s,description:%s,managementIp:%s,state:%s", 
+						inventory.consoleProxyOverriddenIp, inventory.uuid, inventory.description, inventory.managementIp, inventory.state));
+			}
+		}
+		return res;
+	}
+	
+	/**
+	 * 7.4.3 重连控制台代理
+	 * @param agentUuids
+	 * @param sessionId
+	 * @return
+	 * @author Josh
+	 */
+	public ReconnectConsoleProxyAgentAction.Result reconnectConsoleProxyAgent(String agentUuids, String sessionId){
+		ReconnectConsoleProxyAgentAction action = new ReconnectConsoleProxyAgentAction();
+		action.agentUuids = Arrays.asList(agentUuids);
+		action.sessionId = sessionId;
+		ReconnectConsoleProxyAgentAction.Result res = action.call();
+		if (res.error != null) {
+			System.out.println(String.format("reconnectConsoleProxyAgent error code:%s,description:%s,details:%s", res.error.code, res.error.description, res.error.details));
+		} else{
+			Map<String, Object> inventory = res.value.inventory;
+			for (Entry<String, Object> entry: inventory.entrySet()) {
+				System.out.println(String.format("reconnectConsoleProxyAgent successfully key:%s,value:%s", entry.getKey(), entry.getValue()));
+			}
+			
 		}
 		return res;
 	}
